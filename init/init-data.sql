3615b0b0af695c55b016220f508bd07caa0ec87f
mrcsmcl <mrcsmcl@live.com>
2026-03-04 12:45:51 -0300

    Revert "teste"
    This reverts commit 010f7b691a5bfcd14075bae0b586297f03d37587.

--- init/init-data.sql
+++ init/init-data.sql
@@ -1,25 +1,11 @@
 -- Creates the non-root user and grants required privileges.
 -- Executed automatically by the PostgreSQL entrypoint on first init.
-
-\set ON_ERROR_STOP on
+-- Uses \getenv to read container environment variables (requires psql 9.3+).
 
 \getenv non_root_user POSTGRES_NON_ROOT_USER
 \getenv non_root_password POSTGRES_NON_ROOT_PASSWORD
 \getenv db POSTGRES_DB
 
-SELECT format(
-  'CREATE USER %I WITH PASSWORD %L',
-  :'non_root_user',
-  :'non_root_password'
-) \gexec
-
-SELECT format(
-  'GRANT ALL PRIVILEGES ON DATABASE %I TO %I',
-  :'db',
-  :'non_root_user'
-) \gexec
-
-SELECT format(
-  'GRANT CREATE ON SCHEMA public TO %I',
-  :'non_root_user'
-) \gexec
+CREATE USER :"non_root_user" WITH PASSWORD :'non_root_password';
+GRANT ALL PRIVILEGES ON DATABASE :"db" TO :"non_root_user";
+GRANT CREATE ON SCHEMA public TO :"non_root_user";
